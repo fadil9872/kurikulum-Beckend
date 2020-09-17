09f6f241903c32aef7f9c6137fb7cd57629cbf62
romi9872 <adian281r@gmail.com>
2020-09-17 21:42:15 +0700

    latihan-3

--- Sprint-2/Topik-3/latihan-3.1.php
+++ Sprint-2/Topik-3/latihan-3.1.php
@@ -2,33 +2,64 @@
 class Penjumlahan{
 
     public static function luas_persegi($p,$l){
+        if ($p==0||$l==0) {
+            throw new Exception("Bilangan Tidak Boleh '0' ");
+            
+        }
         return ($p*$l);
     }
     public static function keliling_persegi($p,$l){
+        if ($p==0||$l==0) {
+            throw new Exception("Bilangan Tidak Boleh '0' ");
+
         return ($p+$l)*2;
+            }
     }
     public static function luas_segitiga($a,$t){
+        if ($a==0||$t==0) {
+            throw new Exception("Bilangan Tidak Boleh '0' ");
+
         return ($a*$t)/2;
+        }
     }
-    public static function keliling_segitiga($s,$s2,$s3) {
+    public static function keliling_segitiga($s,$s2,$s3){
+        if ($s==0||$s2==0||$s3==0) {
+            throw new Exception("Bilangan Tidak Boleh '0' ");
+        }
         return ($s+$s2+$s3);
+        
     }
     public static function luas_trapesium($a,$b,$t) {
+        if ($a==0||$b==0||$t==0) {
+            throw new Exception("Bilangan Tidak Boleh '0' ");
+
         return ($a+$b)*$t/2;
+        }
     }
     public static function keliling_trapesium($s,$s2,$s3,$s4) {
+        if ($s==0||$s2==0||$s3==0||$s4==0) {
+            throw new Exception("Bilangan Tidak Boleh '0' ");
+
         return ($s+$s2+$s3+$s4);
+        }
     }
     public static function luas_lingkaran($r){
+        if ($r==0) {
+            throw new Exception("Bilangan Tidak Boleh '0' ");
+
         return 3.14*($r*$r);
+        }
     }
     public static function keliling_lingkaran($k){
+        if ($k==0) {
+            throw new Exception("Bilangan Tidak Boleh '0' ");
+
         return 2*3.14*($k);
+        }
     }
 
 }
 $operasi= new Penjumlahan;
-
 echo "==============================\n";
 echo "|     Operasi Penjumlahan    |\n";
 echo "==============================\n";
@@ -51,15 +82,16 @@ switch ($pilih) {
         echo "=================================\n";
         echo "panjang : ";
         $panjang =(int)trim(fgets(STDIN));
-        if ($panjang == 0) {
-            throw new Exception("Panjang Tidak Boleh '0'!");
-        }
         echo "lebar : ";
         $lebar = (int)trim(fgets(STDIN));
-        if ($lebar == 0) {
-            throw new Exception("Lebar Tidak Boleh '0'!");
+        try {
+            $hasil=Penjumlahan::luas_persegi($panjang,$lebar);
+            echo "Luas Persegi Panjang = ".$hasil;
+        } catch (Exception $ex) {
+            echo "Eror Pada".$ex->getFile(). "\nPada Baris".$ex->getLine()."\n";
+            echo $ex->getMessage();
+
         }
-        echo "luas : ".Penjumlahan::luas_persegi($panjang, $lebar)."\n";
         
         break;
     case 2:
@@ -68,15 +100,16 @@ switch ($pilih) {
         echo "===================================\n";
         echo "panjang : ";
         $panjang =(int)trim(fgets(STDIN));
-        if ($panjang == 0) {
-            throw new Exception("Panjang Tidak Boleh '0'!");
-        }
         echo "lebar : ";
         $lebar = (int)trim(fgets(STDIN));
-        if ($lebar == 0) {
-            throw new Exception("Lebar Tidak Boleh '0'!");
+        try {
+            $hasil=Penjumlahan::keliling_persegi($panjang, $lebar);
+            echo "Luas Persegi Panjang = ".$hasil;
+        } catch (Exception $ex) {
+            echo "Eror Pada".$ex->getFile(). "\nPada Baris".$ex->getLine()."\n";
+            echo $ex->getMessage();
+
         }
-        echo "keliling : ".Penjumlahan::keliling_persegi($panjang, $lebar)."\n";
         break;
     
     case 3:
@@ -87,9 +120,16 @@ switch ($pilih) {
         $alas= trim(fgets(STDIN));
         echo "Tinggi : ";
         $tinggi=trim(fgets(STDIN));
-        echo "luasnya : ".Penjumlahan::luas_segitiga($alas, $tinggi)."\n";
+        try {
+            $hasil=Penjumlahan::luas_segitiga($alas,$tinggi);
+            echo "Luasnya = ".$hasil;
+        } catch (Exception $ex) {
+            echo "Eror Pada = ".$ex->getFile(). "\nPada Baris = ".$ex->getLine()."\n";
+            echo $ex->getMessage();
+        }
         break;
     case 4:
+
         echo "====================================\n";
         echo "|         Keliling Segitiga        |\n";
         echo "====================================\n";
@@ -99,7 +139,13 @@ switch ($pilih) {
         $sisi2 = trim(fgets(STDIN));
         echo "Sisi 3 : ";
         $sisi3 = trim(fgets(STDIN));
-        echo "kelilingnya : ".Penjumlahan::keliling_segitiga($sisi,$sisi2,$sisi3)."\n";
+        try {
+            $hasil=Penjumlahan::keliling_segitiga($sisi,$sisi2,$sisi3);
+            echo "Keliling = ".$hasil;
+        } catch (Exception $ex) {
+            echo "Eror Pada = ".$ex->getFile(). "\nPada Baris = ".$ex->getLine()."\n";
+            echo $ex->getMessage();
+        }
         break;
     case 5:
         echo "===================================\n";
@@ -107,11 +153,17 @@ switch ($pilih) {
         echo "===================================\n";
         echo "Sisi Atas : ";
         $sisi_atas= trim(fgets(STDIN));
-        echo "Sisi Bawah";
+        echo "Sisi Bawah : ";
         $sisi_bawah=trim(fgets(STDIN));
         echo "Tinggi : ";
         $tinggi=trim(fgets(STDIN));
-        echo "luasnya : ".Penjumlahan::luas_trapesium($sisi_atas,$sisi_bawah,$tinggi)."\n";
+        try {
+            $hasil=Penjumlahan::luas_trapesium($sisi_atas,$sisi_bawah,$tinggi);
+            echo "Luasnya = ".$hasil;
+        } catch (\Throwable $th) {
+            echo "Eror Pada = ".$th->getFile(). "\nPada Baris = ".$th->getLine()."\n";
+            echo $th->getMessage();
+        }
         
         break;
     case 6:
@@ -126,7 +178,13 @@ switch ($pilih) {
         $sisi3 = trim(fgets(STDIN));
         echo "Sisi 4 : ";
         $sisi4 = trim(fgets(STDIN));
-        echo "kelilingnya : ".Penjumlahan::keliling_trapesium($sisi,$sisi2,$sisi3,$sisi4)."\n";
+        try {
+            $hasil=Penjumlahan::keliling_trapesium($sisi,$sisi2,$sisi3,$sisi4);
+            echo "Kelilingnya = ".$hasil;
+        } catch (Exception $th) {
+            echo "Eror Pada = ".$th->getFile(). "\nPada Baris = ".$th->getLine()."\n";
+            echo $th->getMessage();
+        }
         break;
     case 7:
         echo "===================================\n";
@@ -134,8 +192,13 @@ switch ($pilih) {
         echo "===================================\n";
         echo "r : ";
         $jari=trim(fgets(STDIN));
-        echo "luasnya : ".luas_lingkaran($jari);
-        echo "\n";
+        try {
+            $hasil=Penjumlahan::luas_lingkaran($jari);
+            echo "Luasnya = ".$hasil;
+        } catch (Exception $th) {
+            echo "Eror Pada = ".$th->getFile(). "\nPada Baris = ".$th->getLine()."\n";
+            echo $th->getMessage();
+        }
         break;
     case 8:
         echo "===================================\n";
@@ -143,8 +206,12 @@ switch ($pilih) {
         echo "===================================\n";
         echo "Jari-Jari : ";
         $keliling=trim(fgets(STDIN));
-        echo "kelilingnya : \n".Penjumlahan::keliling_lingkaran($keliling);
-        echo "\n";
+        try {
+            $hasil=Penjumlahan::keliling_lingkaran($keliling);
+        } catch (Exception $th) {
+            echo "Eror Pada = ".$th->getFile(). "\nPada Baris = ".$th->getLine()."\n";
+            echo $th->getMessage();
+        }
         break;
     default:
         echo "Layanan Tidak Tersedia.";
